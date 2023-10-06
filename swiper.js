8c93149284ee44f117fe8011e48d8a8832fe0952
aytac <qehremanova.aytac2003@gmail.com>
2023-10-06 04:40:02 +0400

    sections added

---
+++ swiper.js
@@ -0,0 +1,12 @@
+const bannerSwiper = new Swiper(".banner-swiper", {
+    slidesPerView: 1,
+    
+    // loop: true,
+    // autoplay: {
+    //   delay: 2000,
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+  });
